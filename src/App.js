f7f8cf4311b33d60e3cd7fdc5ea10baa7a55bc08
Sergei Liddell <liddell.s@pm.me>
2021-05-14 12:15:23 -0600

    Implements Router to route to Users page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Users from './User/pages/Users.jsx';
 
 function App() {
+  const styles = {
+    color: 'blue'
+  }
   return (
     <div className="App">
-      <h1 className="hello" style={{ color: 'red', }}>hello</h1>
+      <h1 className="hello" style={styles}>hello</h1>
+      <Router>
+        <Users />
+      </Router>
     </div>
   );
 }
